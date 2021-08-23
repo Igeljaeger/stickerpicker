e67352d658c9bf74e4c8d1869b3907a499a5cd79
hedgehog hunter <igeljaeger@pm.me>
2021-08-23 20:10:41 +0200

    initial commit

---
+++ sticker/version.py
@@ -0,0 +1,6 @@
+# Generated in setup.py
+
+git_tag = None
+git_revision = 'ba009627'
+version = '0.1.0+dev.ba009627'
+linkified_version = '0.1.0+dev.[ba009627](https://github.com/maunium/stickerpicker/commit/ba0096275ce966107b9a00f42e9894af74cebb4c)'
